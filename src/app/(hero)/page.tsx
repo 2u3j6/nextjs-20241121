0e620b91bdeee09ff1781dc25a33792dfc895e41
yu2u3j6 <1398433233@qq.com>
2024-11-21 16:44:54 +0800

    fix 去除为使用的引用

--- src/app/(hero)/page.tsx
+++ src/app/(hero)/page.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import picSrc from '/public/pic.png'
 import Content from "@/components/content";
 import {Metadata} from "next";
-import { Button } from 'antd';
 export const metadata:Metadata = {
   title:'Home'
 }
