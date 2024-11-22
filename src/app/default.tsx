21021974e53bfdd97d74d7b34899a71be23d1807
yu2u3j6 <1398433233@qq.com>
2024-11-22 15:14:53 +0800

    平行路由加default文件

---
+++ src/app/default.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='flex justify-center items-center outline-2 outline-dotted outline-blue-500 w-full my-4 h-[200px] rounded-lg'>@children default</div>
+
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div className='flex justify-center items-center outline-2 outline-dotted outline-blue-500 w-full my-4 h-[200px] rounded-lg'>@team</div>
+    <div className='flex justify-center items-center outline-2 outline-dotted outline-blue-500 w-full my-4 h-[200px] rounded-lg'>@children</div>
 
   )
 }
